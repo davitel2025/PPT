23ab7d4e6d62fc6dfe71894d194bae58e670bb6e
Davi <davititanir@gmail.com>
2025-06-25 14:02:38 -0400

    Version 3

--- script.js
+++ script.js
@@ -1,71 +1,62 @@
-let random=0
+let random = 0
 let computer_choice = ''
 let escolha = ''
 
 
 let score = JSON.parse(localStorage.getItem('score'));
 
-if (!score || score === null){
+if (!score || score === null) {
     score = {
         wins: 0,
         loses: 0,
         ties: 0,
     };
-  
+
 }
-
-/*
-    Diferença entre null e undefined:
-        null --> quero que o valor seja vazio
-        undefined --> o valor vai ser 'default' podendo
-        ser alterado
-        --> na maioria das vezes usamos a mesma função
-    
-
-*/
 
 atualizarPlacarNaTela()
 
-function selecionar_random(botao){
-
+function selecionar_random(botao) {
+    let img_player = document.getElementById('img-result')
+    let img_bot = document.getElementById('img-bot')
     escolha = botao.parentElement.id;
 
     random = Math.random()
-    if(random >= 0 && random <= 1/3){computer_choice = 'Pedra'}
-    else if(random > 1/3 && random <= 1/2){computer_choice = 'Papel'}
-    else if(random > 1/2 && random <= 1){computer_choice = 'Tesoura'}
-    alert(`Você jogou ${escolha} e o oponente ${computer_choice}!`)
+    if (random >= 0 && random <= 1 / 3) { computer_choice = 'Pedra'; img_bot.src = './assets/rock_39413.png'; }
+    else if (random > 1 / 3 && random <= 1 / 2) { computer_choice = 'Papel'; img_bot.src = './assets/hand-paper-svgrepo-com.png';  }
+    else if (random > 1 / 2 && random <= 1) { computer_choice = 'Tesoura'; img_bot.src =  './assets/angellist-svgrepo-com.png';} 
+    mudarModal(escolha, computer_choice, img_player)
+    const modalEscolha = document.getElementById('modal')
+    modalEscolha.classList.add('show');
+
     vitoria()
-
-
-
 
 }
 
-function resetar(){
+function resetar() {
     score.wins = 0
     score.loses = 0
     score.ties = 0
-    localStorage.removeItem('score') 
+    localStorage.removeItem('score')
     atualizarPlacarNaTela()
 }
 
-function vitoria(){
-    
-    if (escolha == computer_choice){
-        score.ties+=1
-        alert(`Empate!\nVitórias: ${score.wins}\nDerrotas: ${score.loses}\nEmpates: ${score.ties}`)
+function vitoria() {
+    const result_game = document.getElementById('result-game')
+    if (escolha == computer_choice) {
+        score.ties += 1
+        result_game.innerHTML = `EMPATE`
 
 
 
-    }else if ((escolha == 'Pedra' && computer_choice == 'Tesoura') || (escolha == 'Tesoura' &&  computer_choice == 'Papel') || (escolha == 'Papel' &&  computer_choice == 'Pedra')){
-        score.wins+=1
-        alert(`Você venceu!\nVitórias: ${score.wins}\nDerrotas: ${score.loses}\nEmpates: ${score.ties}`)
-        
-    }else{
-        score.loses+=1
-        alert(`Você perdeu!\nVitórias: ${score.wins}\nDerrotas: ${score.loses}\nEmpates: ${score.ties}`)
-        
+    } else if ((escolha == 'Pedra' && computer_choice == 'Tesoura') || (escolha == 'Tesoura' && computer_choice == 'Papel') || (escolha == 'Papel' && computer_choice == 'Pedra')) {
+        score.wins += 1
+        result_game.innerHTML = `VOCÊ VENCEU`
+
+    } else {
+        score.loses += 1
+        result_game.innerHTML = `VOCÊ PERDEU`
+
     }
     localStorage.setItem('score', JSON.stringify(score));
     atualizarPlacarNaTela()
@@ -74,6 +65,34 @@ function vitoria(){
 }
 
 function atualizarPlacarNaTela() {
-  document.getElementById('placar').textContent =
-    `PLACAR:\nVitórias: ${score.wins} | Derrotas: ${score.loses} | Empates: ${score.ties}`;
+    document.getElementById('placar').textContent =
+        `PLACAR:\nVitórias: ${score.wins} | Derrotas: ${score.loses} | Empates: ${score.ties}`;
 }
+
+function mudarModal(escolha, computer_choice, img_player){
+    let modalPlayer = document.getElementById('p-result-player')
+    modalPlayer.innerHTML = `Você jogou ${escolha}!`
+    if(escolha === 'Papel'){
+        img_player.src = './assets/hand-paper-svgrepo-com.png'
+    }else if (escolha === 'Pedra'){
+        img_player.src = './assets/rock_39413.png'
+    }else{
+        img_player.src = './assets/angellist-svgrepo-com.png'
+    }
+
+    let modalBot = document.getElementById('p-result-bot')
+    modalBot.innerHTML = `O adversário jogou ${computer_choice}!`
+
+}
+
+function fecharModal(){
+    const modalFechar = document.getElementById('modal')
+    modalFechar.classList.remove('show');
+}
+
+document.addEventListener('click', function(e) {
+  const modalFechar = document.getElementById('modal')
+  if (e.target === modal) {
+    modalFechar.classList.remove('show');
+  }
+});
